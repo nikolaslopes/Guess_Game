88ced33236ca5c31995b63034e82ea97dc45d04f
Nix <nikolaslopes.dev@gmail.com>
2021-06-15 14:01:31 -0300

    add welcome game

---
+++ guess_game.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    cout << "*****************************" << endl;
+    cout << "* Welcome to the Guess Game *" << endl;
+    cout << "*****************************" << endl;
+}
