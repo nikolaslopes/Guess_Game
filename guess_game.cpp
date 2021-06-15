ec0ca1ad9ed7502b853ee46496cf56c53478c2b0
Nix <nikolaslopes.dev@gmail.com>
2021-06-15 14:40:08 -0300

    add secret_number

--- guess_game.cpp
+++ guess_game.cpp
@@ -5,4 +5,7 @@ int main () {
     cout << "*****************************" << endl;
     cout << "* Welcome to the Guess Game *" << endl;
     cout << "*****************************" << endl;
+
+    int secret_number = 69;
+    cout << "O número secreto é " << secret_number << " Não conte para ningupem!" << endl;
 }
