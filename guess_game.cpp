61fb64f652c62e67253ffcc0a56707eac025ff58
Nix <nikolaslopes.dev@gmail.com>
2021-06-16 16:55:56 -0300

    add conditionals

--- guess_game.cpp
+++ guess_game.cpp
@@ -7,5 +7,20 @@ int main () {
     cout << "*****************************" << endl;
 
     int secret_number = 69;
-    cout << "O número secreto é " << secret_number << " Não conte para ningupem!" << endl;
+
+    int shot_number;
+    cout << "Qual o valor do seu chute?";
+    cin >> shot_number;
+
+    cout << "O valor do seu chute é: " << shot_number << endl;
+
+    if (shot_number == secret_number) {
+        cout << "Parabéns, você acertou o número secreto!" << endl;
+    }
+    else if (shot_number > secret_number) {
+        cout << "Seu chute foi maior que o número secreto!" << endl;
+    }
+    else {
+        cout << "Seu chute foi menor que o número secreto!" << endl;
+    }
 }
