70aa8df73341fa8a97500edd3048b88df9ce46ca
Nix <nikolaslopes.dev@gmail.com>
2021-06-18 16:31:22 -0300

    add difficulty

--- guess_game.cpp
+++ guess_game.cpp
@@ -6,17 +6,33 @@ int main () {
     cout << "* Welcome to the Guess Game *" << endl;
     cout << "*****************************" << endl;
 
+    cout << "Escolha o seu nível de dificuldade: " << endl;
+    cout << "Fácil (Press F), Médio (M) ou Difícil (D)" << endl;
+
+    char difficulty;
+    cin >> difficulty;
+
+    int attempts_number;
+
+    if (difficulty == 'F') {
+        attempts_number = 15;
+    }
+    else if (difficulty == 'M') {
+        attempts_number = 10;
+    }
+    else {
+        attempts_number = 5;
+    }
+
     const int SECRET_NUMBER = 42;
 
     bool index = true;
-
     int attempts = 0;
 
     double score = 1000.0;
 
-    while (index) {
+    for (attempts = 1; attempts <= attempts_number; attempts++) {
 
-        attempts ++;
         cout << "Tentativa " << attempts << endl;
 
         int shot_number;
@@ -34,6 +50,7 @@ int main () {
         if (correct_number) {
             cout << "Parabéns, você acertou o número secreto!" << endl;
             index = false;
+            break;
         }
         else if (greater_number) {
             cout << "Seu chute foi maior que o número secreto!" << endl;
@@ -47,9 +64,16 @@ int main () {
     cout << "*** END GAME ***" << endl;
     cout << "****************" << endl;
 
-    cout << "Você acertou o número secreto em " << attempts << " tentativas!" << endl;
+    if (index) {
+        cout << "*** ¯|_( ͡❛ ͜ʖ ͡❛)_/¯ *** " << endl;
+        cout << "*** Game Over ***" << endl;
+        cout << "*** Try Again ***" << endl;
+    }
+    else {
+        cout << "Você acertou o número secreto em " << attempts << " tentativas!" << endl;
 
-    cout.precision(2);
-    cout << fixed;
-    cout << "Sua pontuação foi de: " << score << " pontos" << endl;
+        cout.precision(2);
+        cout << fixed;
+        cout << "Sua pontuação foi de: " << score << " pontos" << endl;
+    }
 }
