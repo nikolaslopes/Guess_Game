adc15dd885b260a56337cd93e431e7889b7e1f05
Nix <nikolaslopes.dev@gmail.com>
2021-06-27 22:09:22 -0300

    add system pause to timeout 1

--- guess_game.cpp
+++ guess_game.cpp
@@ -3,10 +3,13 @@
 #include <ctime>
 using namespace std;
 
-int main () {
+int main()
+{
     cout << "*****************************" << endl;
     cout << "* Welcome to the Guess Game *" << endl;
     cout << "*****************************" << endl;
+
+    system("PAUSE");
 
     cout << "Escolha o seu nível de dificuldade: " << endl;
     cout << "Fácil (Press F), Médio (M) ou Difícil (D)" << endl;
@@ -16,16 +19,19 @@ int main () {
 
     int attempts_number;
 
-    if (difficulty == 'F') {
+    if (difficulty == 'F')
+    {
         attempts_number = 15;
     }
-    else if (difficulty == 'M') {
+    else if (difficulty == 'M')
+    {
         attempts_number = 10;
     }
-    else {
+    else
+    {
         attempts_number = 5;
     }
-    
+
     srand(time(NULL)); //essa função time vai dar o número de segundos desde 1970
     const int SECRET_NUMBER = rand() % 100;
 
@@ -34,15 +40,16 @@ int main () {
 
     double score = 1000.0;
 
-    for (attempts = 1; attempts <= attempts_number; attempts++) {
+    for (attempts = 1; attempts <= attempts_number; attempts++)
+    {
 
         cout << "Tentativa " << attempts << endl;
 
         int shot_number;
         cout << "Qual o valor do seu chute?";
         cin >> shot_number;
-        
-        double score_lost = abs(shot_number - SECRET_NUMBER)/2.0;
+
+        double score_lost = abs(shot_number - SECRET_NUMBER) / 2.0;
         score = score - score_lost;
 
         cout << "O valor do seu chute é: " << shot_number << endl;
@@ -50,15 +57,18 @@ int main () {
         bool correct_number = shot_number == SECRET_NUMBER;
         bool greater_number = shot_number > SECRET_NUMBER;
 
-        if (correct_number) {
+        if (correct_number)
+        {
             cout << "Parabéns, você acertou o número secreto!" << endl;
             index = false;
             break;
         }
-        else if (greater_number) {
+        else if (greater_number)
+        {
             cout << "Seu chute foi maior que o número secreto!" << endl;
         }
-        else {
+        else
+        {
             cout << "Seu chute foi menor que o número secreto!" << endl;
         }
     }
@@ -67,12 +77,14 @@ int main () {
     cout << "*** END GAME ***" << endl;
     cout << "****************" << endl;
 
-    if (index) {
+    if (index)
+    {
         cout << "*** ¯|_( ͡❛ ͜ʖ ͡❛)_/¯ *** " << endl;
         cout << "*** Game Over ***" << endl;
         cout << "*** Try Again ***" << endl;
     }
-    else {
+    else
+    {
         cout << "Você acertou o número secreto em " << attempts << " tentativas!" << endl;
 
         cout.precision(2);
